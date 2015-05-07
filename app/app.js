0f2ba791b5f00c78450e05fd3117159d82ff7659
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-05-07 15:55:18 +0200

    Update to Ember-cli 0.2.3

--- app/app.js
+++ app/app.js
@@ -3,10 +3,12 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
+var App;
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
 Ember.LOG_VERSION = false;
 
-var App = Ember.Application.extend({
+App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
